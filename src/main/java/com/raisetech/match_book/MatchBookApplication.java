63720478bc66a798f40e8784c486ea5ce376531a
mado <>
2022-05-10 01:48:33 +0900

    first commit

---
+++ src/main/java/com/raisetech/match_book/MatchBookApplication.java
@@ -0,0 +1,13 @@
+package com.raisetech.match_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech/match_book/MatchBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech.match_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
